0db2222e294907cbe13536d155baa36924e64490
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-08-14 14:30:39 -0700

    add timer

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,23 @@ var viewScores = document.querySelector('#viewscores');
 var timerEl = document.querySelector('#timer');
 var startButton = document.querySelector('.start-button');
 
+var count = 75;
+var timer;
+
+function countDown() {
+
+    timer = setInterval( function() {
+        
+        count--;
+        timerEl.textContent = 'Time: ' + count;
+
+        if(count === 0) {
+            clearInterval(timer);
+        }
+
+
+    }, 1500);
+
+}
+
+countDown();
